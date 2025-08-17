e1f11d114afe70624e53812f622b98a0bcc4cef3
lochansaroy02 <lochansaroy47@gmail.com>
2025-08-17 23:25:32 +0530

    fixed the errors

--- prisma/migrations/20250813032231_fixed_reciver_name/migration.sql
+++ prisma/migrations/20250813032231_fixed_reciver_name/migration.sql
@@ -1,9 +1,10 @@
-/*
-  Warnings:
+-- /*
+--   Warnings:
 
-  - You are about to drop the column `receiverName` on the `SeizedVehicle` table. All the data in the column will be lost.
+--   - You are about to drop the column `receiverName` on the `SeizedVehicle` table. All the data in the column will be lost.
 
-*/
--- AlterTable
-ALTER TABLE "SeizedVehicle" DROP COLUMN "receiverName",
-ADD COLUMN     "receiverName" TEXT;
+-- */
+-- -- AlterTable
+-- ALTER TABLE "SeizedVehicle" DROP COLUMN "receiverName",
+-- ADD COLUMN     "receiverName" TEXT;
+-- 20250813032231_fixed_reciver_name/migration.sql
