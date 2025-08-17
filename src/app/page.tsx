a51c24de3d790970df0b19a740bab7c0704d1ecc
lochansaroy02 <lochansaroy47@gmail.com>
2025-08-17 23:49:06 +0530

    fixed changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
 
 
   return (
-    <div className="flex min-h-screen items-center gradient justify-center bg-gray-100">
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="  glass-effect p-10 rounded-xl shadow-md w-full max-w-md">
         <h1 className="text-2xl text-blue-100 font-bold mb-6 text-center">Please Login</h1>
         <LoginForm />
