6c7b504f30f23adfdc6248f610faa9224c31040c
lochansaroy02 <lochansaroy47@gmail.com>
2025-08-25 13:08:43 +0530

    added logo at homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Logo from "@/assets/Logo";
 import LoginForm from "@/components/LoginForm";
 import { useTranslations } from "next-intl";
 export default function LoginPage() {
@@ -9,6 +10,9 @@ export default function LoginPage() {
     <div className="flex min-h-screen items-center gradient justify-center ">
       <div className="  glass-effect p-10 rounded-xl shadow-md w-full max-w-md">
         <h1 className="text-2xl text-blue-100 font-bold mb-6 text-center">{t("title")}</h1>
+        <div className='flex justify-center'>
+          <Logo width={120} height={120} />
+        </div>
         <LoginForm />
       </div>
     </div>
