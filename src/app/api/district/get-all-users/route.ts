50b4c77601e7da1c4d76a9ab982d1d5833e542ee
lochansaroy02 <lochansaroy47@gmail.com>
2025-09-04 12:37:27 +0530

    fixed get args

--- src/app/api/district/get-all-users/route.ts
+++ src/app/api/district/get-all-users/route.ts
@@ -2,7 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
+export const GET = async (req: NextRequest) => {
     const { searchParams } = new URL(req.url);
     const districtId = searchParams?.get("districtId");
 
